86d6826b7fd9936ab2f0796b6f449fcd75e89bab
Ethan Richardson <ethanx94@gmail.com>
2018-06-07 20:07:22 -0500

    navbar icons

--- src/js/pages/Main.js
+++ src/js/pages/Main.js
@@ -10,21 +10,29 @@ class Main extends React.Component {
     const sections = [
       {
         content: <PageOne key="Page One" title="Page One" navigator={this.props.navigator} />,
-        title: 'Page One'
+        title: 'Dude',
+        icon: 'md-info'
       },
       {
         content: <PageTwo key="Page Two" title="Page Two" navigator={this.props.navigator} />,
-        title: 'Page Two'
+        title: 'Collection',
+        icon: 'md-link'
       },
       {
         content: <PageThree key="Page Three" title="Page Three" navigator={this.props.navigator} />,
-        title: 'Page Three'
+        title: 'Egg',
+        icon: 'md-info'
+      },
+      {
+        content: <PageThree key="Page Three" title="Page Three" navigator={this.props.navigator} />,
+        title: 'Settings',
+        icon: 'md-settings'
       }
     ];
 
     return sections.map((section) => ({
       content: section.content,
-      tab: <Tab key={section.title} label={section.title} />
+      tab: <Tab key={section.title} label={section.title} icon={section.icon} />
     }));
   }
 
